18f76aaa399173307c80ef328c423149dc1c0e98
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2021-03-14 20:09:58 +0900

    LiveChatAsyncに書き換えてエラーが出ないようにした（反応が少し遅れたかも）

--- discord-getYouTubeChat.py
+++ discord-getYouTubeChat.py
@@ -1,31 +1,50 @@
 import pytchat,discord,asyncio
 from modules import settings
 from logging import basicConfig, getLogger
+from pytchat import LiveChatAsync
 
 basicConfig(level=settings.LOG_LEVEL)
 LOG = getLogger(__name__)
 
 bot = discord.Client(intents=discord.Intents.default())
-chat = pytchat.create(video_id=settings.VIDEO_ID)
 
-async def get_youtube_chat():
-    while chat.is_alive():
-        for c in chat.get().sync_items():
-            youtube_chat_text = f"`{c.datetime}` -> [{c.author.name}] {c.message}"
+async def main():
+    livechat = LiveChatAsync(settings.VIDEO_ID, callback = func)
+    while livechat.is_alive():
+        await asyncio.sleep(1)
+        #other background operation.
 
-            if len(bot.guilds) == 1:
+    # If you want to check the reason for the termination, 
+    # you can use `raise_for_status()` function.
+    try:
+        livechat.raise_for_status()
+    except pytchat.ChatDataFinished:
+        print("Chat data finished.")
+    except Exception as e:
+        print(type(e), str(e))
+
+#callback function is automatically called periodically.
+async def func(chatdata):
+    for c in chatdata.items:
+        youtube_chat_text = f"`{c.datetime}` -> [{c.author.name}] {c.message}"
+        LOG.info(youtube_chat_text)
+        await chatdata.tick_async()
+        if len(bot.guilds) == 1:
                 guild = bot.guilds[0] # BOTに紐づくギルドが1つのみという前提でギルドを取得
-            else:
-                LOG.error(f'BOTに紐づくギルドは1件である必要があります。お手数ですが登録/削除をお願いします。あなたのBOTに紐づくギルドの数：{bot.guilds}')
-                LOG.info('処理を終了します。')
-                return
-            channel = guild.get_channel(settings.DISCORD_CHANNEL_ID)
-            await channel.send(youtube_chat_text)
-
+        else:
+            LOG.error(f'BOTに紐づくギルドは1件である必要があります。お手数ですが登録/削除をお願いします。あなたのBOTに紐づくギルドの数：{bot.guilds}')
+            LOG.info('処理を終了します。')
+            return
+        channel = guild.get_channel(settings.DISCORD_CHANNEL_ID)
+        await channel.send(youtube_chat_text)
 
 @bot.event
 async def on_ready():
     LOG.info('bot ready.')
-    await get_youtube_chat()
+    await main()
 
-bot.run(settings.DISCORD_TOKEN)
+if __name__=='__main__':
+    try:
+        bot.run(settings.DISCORD_TOKEN)
+    except asyncio.exceptions.CancelledError:
+        pass
