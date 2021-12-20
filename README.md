019a5b08f728bb4e2120ac2cd74d1076ee436195
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2021-12-20 22:51:47 +0900

    コマンド追加(set,skip,stop)。並列処理かつ一気に投稿に変更

--- README.md
+++ README.md
@@ -2,13 +2,32 @@
 # このBotについて
 
 - YouTubeの動画IDを`.env`の`VIDEO_ID`に設定しBotを起動すると、ギルドの指定したチャンネルに内容を投稿します。
+  - 設定されていない場合、何もしません($setでvideo_idを設定してください)
+
+## 機能について
+
+### コマンド
+
+#### $skip: チャットをリセットし、読み込む
+
+- チャットが早すぎて読み込みが遅れた場合に使います
+  - 一気に投稿するように変更したので、多分使う機会はないはず
+
+#### $set <video_id>: 読み込み先を別のVideoIDに変更
+
+- 指定されたvideo_idを読み込みます
+- (非同期処理のため、前回まで実行していた処理は急には止まれません。時間をおいて止まります)
+
+#### $stop: 処理を停止
+
+- 処理を停止します
 
 ## 環境構築について
 
 ### 前提
 
 1. pythonの3系がインストールされていること
-2. gitがインストールされていること
+2. git, poetryがインストールされていること
 3. Discordの管理者（もしくは、Webhookを作成してもらえる立場）であること
 4. Botに紐づくギルドは1である必要があります(複数あった場合の対応をしていないため)
 
@@ -26,6 +45,8 @@
 
 ### 準備
 
+#### 共通手順
+
 1. 必要な情報を保管する、`.env`ファイルを作成
    1. `.env.sample`をコピーし、名前を`.env`にする
    2. `.env`の「YOUR_DISCORD_TOKEN_IS_HERE」に、**Botのトークンを入力する**
@@ -33,13 +54,15 @@
    4. `.env`の「YOUR_YOUTUBE_VIDEO_ID_IS_HERE」に、**YouTubeの動画IDを入力**し、保存する
       1. たとえば、「<https://www.youtube.com/watch?v=dVjfdgGygo0>」の場合、`dVjfdgGygo0`が動画ID
    5. (オプション)`.env`の「CSV_OUTPUT_FLAG」に、csvとして保存したくない場合、`FALSE`を記載(環境変数が存在しない場合もFALSE扱い)
-2. pythonの仮想環境を作成
-   1. python3 -m venv venv
-   2. source venv/bin/activate
-3. 必要なパッケージを`requirements.txt`からインストール
+
+#### venvを使う手順
+
+1. pythonの仮想環境を作成
+   1. `python3 -m venv venv`
+2. 必要なパッケージを`requirements.txt`からインストール
    1. `pip install -r requirements.txt`
 
-### Botの起動/停止
+3. Botの起動/停止
 
 - `python discord-getYoutubeChat.py`で起動する(`INFO:Test:bot ready.`と記載されたら起動成功)
 
@@ -47,7 +70,27 @@
 (venv) mac-mini:discord-getYoutubeChat mac_mini$ python discord-getYoutubeChat.py WARNING:discord.client:PyNaCl is not installed, voice will NOT be supported
 INFO:discord.client:logging in using static token
 ...（中略）...
-INFO:__main__:bot ready.
+INFO:discord-getYouTubeChat:We have logged in as xxxxxxx
+```
+
+- `Ctrl + C`: 停止(YouTubeでコメントを拾ったタイミングで、Discordに投稿せず終了される)
+
+#### poetryを使う手順
+
+1. poetryでモジュールのインストール(poetry.lockのものをインストール)
+   1. `poetry install`
+2. Botの起動/停止
+
+- `poetry run python discord-getYoutubeChat.py`で起動する(`INFO:Test:bot ready.`と記載されたら起動成功)
+
+```sh
+(venv) mac-mini:discord-getYouTubeChat mac_mini$ poetry run python discord-getYouTubeChat.py 
+WARNING:discord.client:PyNaCl is not installed, voice will NOT be supported
+INFO:discord-getYouTubeChat:cogを読むぞ！
+INFO:cogs.getYouTubeChatCog:GetYouTubeChatCogを読み込む！
+INFO:discord.client:logging in using static token
+...（中略）...
+INFO:discord-getYouTubeChat:We have logged in as xxxxxxx
 ```
 
 - `Ctrl + C`: 停止(YouTubeでコメントを拾ったタイミングで、Discordに投稿せず終了される)
--- cogs/getYouTubeChatCog.py
+++ cogs/getYouTubeChatCog.py
@@ -10,11 +10,16 @@ LOG = getLogger(__name__)
 
 # コグとして用いるクラスを定義。
 class GetYouTubeChatCog(commands.Cog):
+    DISCORD_MAX_MESSAGE_SIZE = 2000
+
     def __init__(self, bot):
         self.bot = bot
         self.queues = queue.Queue()
         self.livechat = None
         self.channel = None
+        self.video_id = None
+        self.last_data = None
+        self.running = False
 
     def prepare(self):
         if len(self.bot.guilds) == 1:
@@ -23,21 +28,70 @@ class GetYouTubeChatCog(commands.Cog):
             LOG.error(f'BOTに紐づくギルドは1件である必要があります。お手数ですが登録/削除をお願いします。あなたのBOTに紐づくギルドの数：{self.bot.guilds}')
             LOG.info('処理を終了します。')
             return
+
         if setting.DISCORD_CHANNEL_ID:
             self.channel = guild.get_channel(setting.DISCORD_CHANNEL_ID)
+        if setting.VIDEO_ID:
+            self.video_id = setting.VIDEO_ID
+        else:
+            LOG.warning('環境変数のVIDEO_IDが設定されていないため何も読み込みません')
 
     # 読み込まれた時の処理
     @commands.Cog.listener()
     async def on_ready(self):
         LOG.debug(self.bot.guilds)
         self.prepare()
+        LOG.info('処理を始めます')
         await self.main()
 
+    @commands.command(aliases=['sk'], description='チャットをスキップします')
+    async def skip(self, ctx):
+        LOG.info('チャットをスキップします')
+        if self.livechat is not None and self.livechat.is_alive():
+            self.livechat.terminate()
+        if self.video_id is not None:
+            self.livechat = LiveChatAsync(self.video_id, callback = self.func)
+        self.queues = queue.Queue()
+
+    @commands.command(aliases=['st'], description='指定されたVideoidを読み込みます')
+    async def set(self, ctx, video_id:str=None):
+        LOG.info('指定されたVideoidを読み込みます')
+        self.channel = ctx.channel
+
+        # 実行中であれば止める
+        if self.livechat is not None and self.livechat.is_alive():
+            self.livechat.terminate()
+
+        # 引数があればvideo_idを設定
+        if video_id is not None:
+            self.video_id = video_id
+
+        # 準備する
+        self.queues = queue.Queue()
+        if self.video_id is not None:
+            LOG.info('ココきた')
+
+            # 止まっているだけならセット。もし開始されてない場合、処理を開始
+            if self.running:
+                self.livechat = LiveChatAsync(self.video_id, callback = self.func)
+            else:
+                LOG.info('開始されていないため、今から処理を始めます')
+                await self.main()
+
+    @commands.command(aliases=['tomeru'], description='チャットをストップします')
+    async def stop(self, ctx):
+        if self.livechat is not None and self.livechat.is_alive():
+            LOG.info('チャットをストップします')
+            self.livechat.pause()
+
     async def main(self):
-        self.livechat = LiveChatAsync(setting.VIDEO_ID, callback = self.func)
+        if self.video_id is None:
+            return
+        self.livechat = LiveChatAsync(self.video_id, callback = self.func)
+        self.running = True
         while self.livechat.is_alive():
-            await asyncio.sleep(0.01)
-            #other background operation.
+            await asyncio.sleep(0.001)
+            await self.task()
 
         # If you want to check the reason for the termination, 
         # you can use `raise_for_status()` function.
@@ -55,15 +109,17 @@ class GetYouTubeChatCog(commands.Cog):
             amount = f'({c.amountString})' if c.amountString != '' else ''
             youtube_chat_text = f"`{c.datetime}` -> [{c.author.name}] {c.message}{amount}"
             put_queue_task = asyncio.create_task(self.put_queue(youtube_chat_text)) if youtube_chat_text is not None else put_queue_task
-            gather_queue_task = asyncio.create_task(self.gather_queue(self.queues))
             file_write_task = asyncio.create_task(self.file_write(c))
-            gathered_data = await gather_queue_task
             await file_write_task,put_queue_task
 
-            if gathered_data is not None and gathered_data != '':
-                log_write_task = asyncio.create_task(self.log_write(gathered_data))
-                channel_send_task = asyncio.create_task(self.channel.send(gathered_data))
-                await channel_send_task, log_write_task
+    async def task(self):
+        gather_queue_task = asyncio.create_task(self.gather_queue(self.queues))
+        gathered_data = await gather_queue_task
+
+        if gathered_data is not None and gathered_data != '':
+            log_write_task = asyncio.create_task(self.log_write(gathered_data))
+            channel_send_task = asyncio.create_task(self.channel.send(gathered_data))
+            await channel_send_task, log_write_task
 
     async def put_queue(self, text):
         self.queues.put(text)
@@ -81,9 +137,20 @@ class GetYouTubeChatCog(commands.Cog):
     async def gather_queue(self, queue_data):
         if queue_data.empty():
             return
-        gathered_data = ''
+        if self.last_data is not None:
+            gathered_data = self.last_data
+            self.last_data = None
+        else:
+            gathered_data = ''
+
         while not queue_data.empty():
-            gathered_data += f'{queue_data.get()}'
+            self.last_data = f'{queue_data.get()}\n'
+            # Discordに投稿できないサイズになってないか確認
+            if len(gathered_data+self.last_data) < (self.DISCORD_MAX_MESSAGE_SIZE - 100):
+                gathered_data += f'{self.last_data}'
+                self.last_data = None
+            else:
+                break
         return gathered_data
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
