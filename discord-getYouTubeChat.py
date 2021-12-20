0e067883a3b0544ce362ad46937387dbdff27066
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2021-12-20 20:47:37 +0900

    asyncio.create_taskを多用して動きを速くしたつもり

--- discord-getYouTubeChat.py
+++ discord-getYouTubeChat.py
@@ -1,4 +1,4 @@
-import pytchat,discord,asyncio,csv,os
+import pytchat,discord,asyncio,csv,os,queue
 from modules import settings
 from logging import basicConfig, getLogger
 from pytchat import LiveChatAsync
@@ -8,11 +8,12 @@ basicConfig(level=settings.LOG_LEVEL)
 LOG = getLogger(__name__)
 
 bot = discord.Client(intents=discord.Intents.default())
+queues = queue.Queue()
 
 async def main():
     livechat = LiveChatAsync(settings.VIDEO_ID, callback = func)
     while livechat.is_alive():
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.01)
         #other background operation.
 
     # If you want to check the reason for the termination, 
@@ -26,24 +27,51 @@ async def main():
 
 #callback function is automatically called periodically.
 async def func(chatdata):
+    if len(bot.guilds) == 1:
+        guild = bot.guilds[0] # BOTに紐づくギルドが1つのみという前提でギルドを取得
+    else:
+        LOG.error(f'BOTに紐づくギルドは1件である必要があります。お手数ですが登録/削除をお願いします。あなたのBOTに紐づくギルドの数：{bot.guilds}')
+        LOG.info('処理を終了します。')
+        return
+    if settings.DISCORD_CHANNEL_ID:
+        channel = guild.get_channel(settings.DISCORD_CHANNEL_ID)
+
     for c in chatdata.items:
-        youtube_chat_text = f"`{c.datetime}` -> [{c.author.name}] {c.message}"
-        LOG.info(youtube_chat_text)
-        await chatdata.tick_async()
-        if len(bot.guilds) == 1:
-                guild = bot.guilds[0] # BOTに紐づくギルドが1つのみという前提でギルドを取得
-        else:
-            LOG.error(f'BOTに紐づくギルドは1件である必要があります。お手数ですが登録/削除をお願いします。あなたのBOTに紐づくギルドの数：{bot.guilds}')
-            LOG.info('処理を終了します。')
-            return
-        if settings.DISCORD_CHANNEL_ID:
-            channel = guild.get_channel(settings.DISCORD_CHANNEL_ID)
-            await channel.send(youtube_chat_text)
-        if settings.CSV_OUTPUT_FLAG:
-            FILE_PATH = join(dirname(__file__), 'modules' + os.sep + 'files' + os.sep + 'csv' + os.sep + settings.VIDEO_ID + '.csv')
-            with open(FILE_PATH, 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([c.datetime, c.type, c.id, c.message, c.elapsedTime, c.amountString, c.currency, c.amountValue, c.author.name])
+        amount = f'({c.amountString})' if c.amountString != '' else ''
+        youtube_chat_text = f"`{c.datetime}` -> [{c.author.name}] {c.message}{amount}"
+        put_queue_task = asyncio.create_task(put_queue(youtube_chat_text)) if youtube_chat_text is not None else put_queue_task
+        gather_queue_task = asyncio.create_task(gather_queue(queues))
+        file_write_task = asyncio.create_task(file_write(c))
+        gathered_data = await gather_queue_task
+        await file_write_task,put_queue_task
+
+        if gathered_data is not None and gathered_data != '':
+            log_write_task = asyncio.create_task(log_write(gathered_data))
+            channel_send_task = asyncio.create_task(channel.send(gathered_data))
+            await channel_send_task, log_write_task
+
+async def put_queue(text):
+    queues.put(text)
+
+async def log_write(text):
+    LOG.info(text)
+
+async def file_write(c):
+    if settings.CSV_OUTPUT_FLAG:
+        FILE_PATH = join(dirname(__file__), 'modules' + os.sep + 'files' + os.sep + 'csv' + os.sep + settings.VIDEO_ID + '.csv')
+        with open(FILE_PATH, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([c.datetime, c.type, c.id, c.message, c.elapsedTime, c.amountString, c.currency, c.amountValue, c.author.name])
+
+async def gather_queue(queue_data):
+    if queue_data.empty():
+        return
+    gathered_data = ''
+    # for data in queue_data.get():
+    #     gathered_data += f'{dat
+    while not queue_data.empty():
+        gathered_data += f'{queue_data.get()}'
+    return gathered_data
 
 @bot.event
 async def on_ready():
